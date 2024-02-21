4d159579f9048ef857e7dd110c91a8b4b59c7bf1
marcosmiguel <marcos.a.miguel@gmail.com>
2024-02-20 22:52:47 -0300

    Ajustes

--- src/test/java/padroesestruturais/proxy/AlunoProxyTest.java
+++ src/test/java/padroesestruturais/proxy/AlunoProxyTest.java
@@ -1,11 +1,6 @@
 package padroesestruturais.proxy;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class AlunoProxyTest {
 
